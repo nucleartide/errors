6cb6f96bd3eea7a50604f6614169e8d1d09a46b1
Jason Tu <jasontu4@gmail.com>
2017-09-28 02:12:32 -0400

    Update mix.exs for publishing

--- mix.exs
+++ mix.exs
@@ -1,17 +1,22 @@
-defmodule Error.Mixfile do
+defmodule Errors.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :error,
+      app: :errors,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+
+      description: description(),
+      package: package(),
       deps: deps(),
+      source_url: "https://github.com/nucleartide/errors",
+
       test_paths: ["."],
       elixirc_paths: File.ls!()
         |> Enum.filter(fn path -> File.dir?(path) end)
-        |> Enum.filter(fn path -> not Enum.member?([".git", "_build", "deps"], path) end),
+        |> Enum.filter(&include_file?/1),
     ]
   end
 
@@ -28,5 +33,26 @@ defmodule Error.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp description() do
+    """
+    Errors is an Elixir package that adds debugging context to error reasons. It
+    is meant to be used in the tagged tuple style of error handling, where a
+    function may return `{:ok, result}` or `{:error, reason}`.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Jason Tu"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nucleartide/errors"},
+      files: File.ls!() |> Enum.filter(&include_file?/1)
+    ]
+  end
+
+  defp include_file?(path) do
+    not Enum.member?([".git", "_build", "deps"], path)
   end
 end
